14a6417e452009fc9161e62fc4e8677450c720ad
shawn <1310179113@qq.com>
2018-11-26 23:07:07 +0800

    test instructions

--- ex3/ex3.cache/ip/2018.1/f5ca7a33858a57d0/inst_mem_stub.vhdl
+++ ex3/ex3.cache/ip/2018.1/f5ca7a33858a57d0/inst_mem_stub.vhdl
@@ -1,18 +1,18 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
--- Tool Version: Vivado v.2018.1 (lin64) Build 2188600 Wed Apr  4 18:39:19 MDT 2018
--- Date        : Mon Nov 26 22:01:15 2018
--- Host        : shawn-All-Series running 64-bit Ubuntu 18.04.1 LTS
--- Command     : write_vhdl -force -mode synth_stub
---               /home/shawn/workspace/ComputerDesignAndOriganization/CPU/CPU.srcs/sources_1/ip/inst_rom/inst_rom_stub.vhdl
--- Design      : inst_rom
+-- Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+-- Date        : Mon Nov 19 16:17:57 2018
+-- Host        : DESKTOP-FAD05NK running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ inst_mem_stub.vhdl
+-- Design      : inst_mem
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7a100tcsg324-1
 -- --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity inst_rom is
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
   Port ( 
     clka : in STD_LOGIC;
     ena : in STD_LOGIC;
@@ -20,9 +20,9 @@ entity inst_rom is
     douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
   );
 
-end inst_rom;
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
 
-architecture stub of inst_rom is
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
